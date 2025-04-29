d54187fd91d5fb50eda77045cfd0d8eff37fbe91
Nathan Rivollier <nathan@mac.home>
2025-04-29 18:20:40 +0200

    part 2: Optimisation de l'exercice

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { readFileSync } from "fs";
 const test = readFileSync("test.txt", "utf8");
 const final = readFileSync("final.txt", "utf8");
 
-const day = 80;
+const day = 256;
 
 const newDay = (fish) => {
   const staticLength = fish.length;
@@ -19,16 +19,35 @@ const newDay = (fish) => {
 
 const part1 = (data) => {
   let result = data.split(",").map(Number);
-  console.log(`Initial state: ${result}`);
 
   for (let i = 1; i <= day; i++) {
     newDay(result);
-
-    //console.log(`After  ${i} day : ${result}`);
   }
 
   return `Il y a : ${result.length} lanterfish`;
 };
-console.log(part1(final));
 
-console.log({ test, final });
+//console.log(part1(final));
+
+const part2 = (data) => {
+  const ages = data
+    .split(",")
+    .map(Number)
+    .reduce((acc, curr) => {
+      acc[curr] += 1;
+      return acc;
+    }, Array.from({ length: 9 }).fill(0));
+
+  for (let i = 0; i < day; i++) {
+    const day0 = ages.shift();
+
+    ages.push(day0);
+    ages[6] += day0;
+  }
+
+  return `Il y a : ${ages.reduce((acc, num) => acc + num, 0)} lanterfish`;
+};
+
+console.log(part2(final));
+
+//console.log({ test, final });
